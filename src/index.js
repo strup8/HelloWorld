35c4748f92fa0e5506079cab126f618141d2b720
strup8 <i.am@vaclavstrnad.cz>
2018-09-19 10:10:55 +0200

    Můj první commit
    Změnili jsme text a přidali obrázek.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
+<h1>Dobrý den.</h1>
 <div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
+  Co člověk, to hádanka.
 </div>
 `;
